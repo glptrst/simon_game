0c88a6e097f60c0d8b8337776eeac1e2905118ba
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-07-05 16:14:36 +0200

    Play audio when button is clicked.

--- js/simon_game.js
+++ js/simon_game.js
@@ -13,16 +13,28 @@
 */
 
 window.onload = function() {
+    var isGameOn = false;
+
+    var counter = 0;
+
+    var strict = false;
+    
     var isUserTurn = true;
+
+    var redAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+    var yellowAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+    var greenAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+    var blueAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
     
     var redButton =  document.getElementById('redButton');
     redButton.addEventListener('mouseup', function() {
 	if (isUserTurn) {
 	    var btn = this;
+	    redAudio.play();
 	    btn.style['background-color']= '#F40000';
 	    setTimeout(function() {
 		btn.style['background-color'] = '#CC0000';
-	    }, 700);
+	    }, 500);
 	}
     });
 
@@ -30,10 +42,11 @@ window.onload = function() {
     yellowButton.addEventListener('mouseup', function() {
 	if (isUserTurn) {
 	    var btn = this;
+	    yellowAudio.play();
 	    btn.style['background-color']= '#FFF028';
 	    setTimeout(function() {
 		btn.style['background-color'] = '#E5D600';
-	    }, 700);
+	    }, 500);
 	}
     });
 
@@ -41,6 +54,7 @@ window.onload = function() {
     greenButton.addEventListener('mouseup', function() {
 	if (isUserTurn) {
 	    var btn = this;
+	    greenAudio.play();
 	    btn.style['background-color']= '#31FF2D';
 	    setTimeout(function() {
 		btn.style['background-color'] = '#029E00';
@@ -52,6 +66,7 @@ window.onload = function() {
     blueButton.addEventListener('mouseup', function() {
 	if (isUserTurn) {
 	    var btn = this;
+	    blueAudio.play();
 	    btn.style['background-color']= '#2861FF';
 	    setTimeout(function() {
 		btn.style['background-color'] = '#003ADB';
