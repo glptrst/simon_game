c454d80b546b29470521b065555d7474519e2e21
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-07-10 17:12:04 +0200

    create array of button objects

--- js/simon_game.js
+++ js/simon_game.js
@@ -1,75 +1,89 @@
-/*
-  red: #CC0000;
-  ligth-red: #F40000;
-
-  yellow: #E5D600;
-  light-yellow: #FFF028;
-
-  green: #029E00;
-  light green: #31FF2D;
-
-  blue: #003ADB;
-  light-blue: #2861FF;
-*/
-
 window.onload = function() {
-    var buttons= {
-	redButton: {
-	    id: 'redButton',
-	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
-	},
-	yellowButton: {
-	    id: 'yellowButton',
-	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
-	},
-	greenButton: {
-	    id: 'greenButton',
-	    audio :new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
-	},
-	blueButton: {
-	    id: 'blueButton',
-	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
-	}
-    };
-    
-    var gameStack = ['redButton', 'yellowButton'];
-    
-    var startBtn = document.getElementById('start');
-    startBtn.addEventListener('click', startGame);
 
-    function startGame() {
-	for (let i = 0; i < gameStack.length; i++) {
+    // random game sequence 
+    var sequence = randomSequence();
+    
+    var buttons= [
+    	{
+    	    id: 'btn0',
+	    node: document.getElementById('btn0'),
+    	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+	    color: '#CC0000',
+	    lightColor: '#F40000',
+	    activate: function() {
+		this.node.setAttribute('style', 'background-color: ' + this.lightColor);
+	    },
+	    deactivate: function() {
+		this.node.setAttribute('style', 'background-color: ' + this.color);
+	    }
+    	},
+    	{
+    	    id: 'btn1',
+	    node: document.getElementById('btn1'),
+    	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+	    color: '#E5D600',
+	    lightColor: '#FFF028',
+	    activate: function() {
+		this.node.setAttribute('style', 'background-color: ' + this.lightColor);
+	    },
+	    deactivate: function () {
+		this.node.setAttribute('style', 'background-color: ' + this.color);
+	    }
+    	},
+    	{
+    	    id: 'btn2',
+	    node: document.getElementById('btn2'),
+    	    audio :new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+	    color: '#029E00',
+	    lightColor: '#31FF2D',
+	    activate: function() {
+		this.node.setAttribute('style', 'background-color: ' + this.lightColor);
+	    },
+	    deactivate: function () {
+		this.node.setAttribute('style', 'background-color: ' + this.color);
+	    }
+    	},
+    	{
+    	    id: 'btn3',
+	    node: document.getElementById('btn3'),
+    	    audio: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+	    color: '#003ADB',
+	    lightColor: '#2861FF',
+	    activate: function() {
+		this.node.setAttribute('style', 'background-color: ' + this.lightColor);
+	    },
+	    deactivate: function () {
+		this.node.setAttribute('style', 'background-color: ' + this.color);
+	    }
+    	}
+    ];
 
+    // return array of random sequence of 20 buttons
+    function randomSequence() {
+	var arr = [];
+	for (var i = 0; i < 20; i ++) {
+	    arr.push(randomRange(0,3));
 	}
+	return arr;
+    }
+    
+    // generate random number between a range
+    function randomRange(myMin, myMax) {
+    	return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
     }
 
-    // var isGameOn = false;
 
-    // var counter = 0;
-    // var gameStack = [];
     
-    // var buttons = ['redButton', 'yellowButton', 'greenButton', 'blueButton'];
 
-    // var strict = false;
+
     
-    // var isUserTurn = false;
-
+    
+    
     // var redAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
     // var yellowAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
     // var greenAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
     // var blueAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
-    // function startGame() {
-    // 	//play old buttons
-    // 	for (var i = 0; i < counter; i++) {
-	    
-    // 	}
-    // 	//play a new button 
-    // 	var randomButton = buttons[randomRange(0, 3)];
-    // 	counter++;
-    // 	gameStack.push(randomButton);
-    // }
-    
     // var redButton =  document.getElementById('redButton');
     // redButton.addEventListener('mouseup', function() {
     // 	if (isGameOn && isUserTurn) {
@@ -129,11 +143,6 @@ window.onload = function() {
     // 	    isGameOn = true;
     // 	    console.log('is true!');
     // 	}
-    // }
-
-    // // generate random number between a range
-    // function randomRange(myMin, myMax) {
-    // 	return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
     // }
 
 };
